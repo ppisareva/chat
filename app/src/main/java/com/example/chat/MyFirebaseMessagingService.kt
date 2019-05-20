3ddc840ccb8124e77a2420c95997e5ce7e996998
polinasliusarchuk <polina.pisareva@gmail.com>
2019-05-20 09:14:52 +0300

    notification to wearable devices

--- app/src/main/java/com/example/chat/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/chat/MyFirebaseMessagingService.kt
@@ -6,21 +6,43 @@ import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
 import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
+val GROUP_KEY_EMAILS = "group_key_emails"
+
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     val TAG = "Service"
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
-        // TODO: Handle FCM messages here.
+      
         // If the application is in the foreground handle both data and notification messages here.
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated.
         Log.d(TAG, "From: " + remoteMessage!!.from)
         Log.d(TAG, "Notification Message Body: " + remoteMessage.notification!!.body!!)
         sendNotification(remoteMessage)
+        // sending notification to watch
+        sendNotificationToWearable(remoteMessage)
     }
+
+    private fun sendNotificationToWearable(remoteMessage: RemoteMessage) {
+
+
+        // Build the notification, setting the group appropriately
+        val notif = NotificationCompat.Builder(applicationContext)
+            .setContentTitle("New mail from ${remoteMessage!!.from}")
+            .setContentText(remoteMessage.notification!!.body)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setGroup(GROUP_KEY_EMAILS)
+            .build()
+
+// Issue the notification
+        val notificationManager = NotificationManagerCompat.from(applicationContext)
+        notificationManager.notify(remoteMessage.messageId.hashCode(), notif)
+    }
+
     private fun sendNotification(remoteMessage: RemoteMessage) {
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
